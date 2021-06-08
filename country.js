79db0a8dc693d5b7cd6f4ce78398a93229e8c68a
Amin Gharibshah <amingharibshah@gmail.com>
2021-06-08 22:23:26 +0430

    delete jquery and add all options directly in html

--- country.js
+++ country.js
@@ -1,14 +1,3 @@
-$(function () {
-  let countryOpts;
-  $.getJSON("/country-by-name.json", function (data) {
-    $.each(data, function (i, valueOfElement) {
-      console.log(valueOfElement.country);
-      countryOpts += `<option  value="${valueOfElement.country}">${valueOfElement.country}</option>`;
-    });
-    $("#country").html(countryOpts);
-  });
-});
-
 document.querySelector(".search-btn").addEventListener("click", function () {
   localStorage.setItem("country", document.getElementById("country").value);
 });
